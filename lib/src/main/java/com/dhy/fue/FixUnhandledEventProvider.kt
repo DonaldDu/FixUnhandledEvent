62bb99efd722c4506bfafe0561936117ea2be39f
donald <onsemise@qq.com>
2019-08-16 09:56:06 +0800

    init when Build.VERSION.SDK_INT < 28

--- lib/src/main/java/com/dhy/fue/FixUnhandledEventProvider.kt
+++ lib/src/main/java/com/dhy/fue/FixUnhandledEventProvider.kt
@@ -2,12 +2,13 @@ package com.dhy.fue
 
 import android.content.Context
 import android.content.pm.ProviderInfo
+import android.os.Build
 import android.support.v4.content.FileProvider
 
 
 class FixUnhandledEventProvider : FileProvider() {
     override fun attachInfo(context: Context, info: ProviderInfo) {
         super.attachInfo(context, info)
-        DexPlugin.load(context)
+        if (Build.VERSION.SDK_INT < 28) DexPlugin.load(context)
     }
 }
